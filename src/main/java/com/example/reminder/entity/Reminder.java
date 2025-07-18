f5a7f18f3c793e8240698d5a1b6c9bcdb58efa6a
Lubov Avduhova <loveavduhova@gmail.com>
2025-07-18 20:57:01 +0700

    Созданы entity, добавлена миграция liquibase

---
+++ src/main/java/com/example/reminder/entity/Reminder.java
@@ -0,0 +1,38 @@
+package com.example.reminder.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Max;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reminder")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//todo можно ли использовать @Data?
+public class Reminder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Max(value = 255, message = "Краткое описание должно быть больше 255 символов")
+    private String title;
+
+    @Max(value = 4096, message = "Полное описание должно быть больше 4096 символов")
+    private String description;
+
+    @Future(message = "Неправильно введена дата")
+    private LocalDateTime remind;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+}
---
+++ src/main/java/com/example/reminder/entity/User.java
@@ -0,0 +1,40 @@
+package com.example.reminder.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String username;
+
+    @Min(value = 5, message = "Телеграм Id должен содержать более 5 символов")
+    @Max(value = 32, message = "Телеграм Id должен содержать не более 32 символов")
+    @Column(name = "telegram_id")
+    private String telegramId;
+
+    @Email(message = "Неправильно введена почта")
+    private String email;
+
+    //todo тут точно all или отдельно прописать какие нужно?
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reminder> reminders;
+}
