c4249c63aba7f4004264ebb117fa57d10ed0fb5c
Lubov Avduhova <loveavduhova@gmail.com>
2025-07-26 21:01:52 +0700

    Добавлен тест на класс ViewReminderController плюс реализация /list

--- src/main/java/com/example/reminder/controller/ViewReminderController.java
+++ src/main/java/com/example/reminder/controller/ViewReminderController.java
@@ -1,5 +1,38 @@
 package com.example.reminder.controller;
 
+import com.example.reminder.dto.response.FullReminderResponse;
+import com.example.reminder.service.reminder.ReminderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/list")
+@RequiredArgsConstructor
 public class ViewReminderController {
-    //todo  //    // Список должен выводиться с пагинацией https://domain/api/v1/list  { total, current }
+
+    private final ReminderService reminderService;
+
+    /**
+     * Получение списка с пагинацией и сортировкой
+     * @param pageable параметры пагинации и сортировки
+     * @return объект с total и current
+     */
+    @GetMapping()
+    public Map<String, Object> getPaginatedList(@PageableDefault(size = 10) Pageable pageable) {
+        Page<FullReminderResponse> page = reminderService.findAllRemindersBySort(pageable);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("total", page.getTotalElements());
+        response.put("current", page.getContent());
+
+        return response;
+    }
 }
---
+++ src/test/java/com/example/reminder/controller/ViewReminderControllerTest.java
@@ -0,0 +1,80 @@
+package com.example.reminder.controller;
+
+import com.example.reminder.dto.response.FullReminderResponse;
+import com.example.reminder.service.reminder.ReminderService;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest(controllers = ViewReminderController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class ViewReminderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ReminderService reminderService;
+
+    @Test
+    public void ListController_GetPaginatedList_ReturnsTotalAndCurrentList() throws Exception {
+        // arrange
+        LocalDateTime remindAfter = LocalDateTime.now().plusMinutes(10);
+        LocalDateTime remindBefore = remindAfter.minusDays(2);
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        FullReminderResponse firstResponse = FullReminderResponse.builder()
+                .title("Помыть машину")
+                .description("Найти свободное время")
+                .remind(remindBefore.plusDays(1))
+                .build();
+
+        FullReminderResponse secondResponse = FullReminderResponse.builder()
+                .title("Приготовить ужин")
+                .description("За 30 минут")
+                .remind(remindAfter)
+                .build();
+
+        List<FullReminderResponse> responses = List.of(firstResponse, secondResponse);
+        Page<FullReminderResponse> responsesPage = new PageImpl<>(responses, pageable, responses.size());
+
+        given(reminderService.findAllRemindersBySort(ArgumentMatchers.any(Pageable.class)))
+                .willReturn(responsesPage);
+
+        // act
+        ResultActions actions = mockMvc.perform(get("/list")
+                .param("page", String.valueOf(pageable.getPageNumber()))
+                .param("size", String.valueOf(pageable.getPageSize()))
+                .param("sort", "title,asc"));
+
+        // assert
+        actions.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total").value(responses.size()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.current", hasSize(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.current[0].title", CoreMatchers.is("Помыть машину")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.current[0].description", CoreMatchers.is("Найти свободное время")));
+
+        verify(reminderService, times(1)).findAllRemindersBySort(ArgumentMatchers.any(Pageable.class));
+    }
+}
